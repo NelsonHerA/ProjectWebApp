2c0fe1eaec00d5d54aaa724109e6d5104d0162db
Juan Dolci <jdolcics@gmail.com>
2023-06-19 17:31:54 -0500

    Se agrego el marco principal

--- app/urls.py
+++ app/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+# from django.contrib.auth import urls
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("accounts/login/", auth_views.LoginView.as_view(redirect_authenticated_user=True)),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),
     path('', include("home.urls")),
 ]
